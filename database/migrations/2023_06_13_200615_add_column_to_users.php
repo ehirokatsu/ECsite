9f7da238ae493f8fab559f624ae7ad6fc7170746
hiro <e.hirokatsu@gmail.com>
2023-06-13 21:47:36 +0900

    usersテーブルに住所、電話番号のカラムを追加。

---
+++ database/migrations/2023_06_13_200615_add_column_to_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //追加カラム
+            $table->string('postal_code');
+            $table->string('address_1');
+            $table->string('address_2');
+            $table->string('address_3');
+            $table->string('phone_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -34,11 +34,62 @@
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+                            name="postalCode" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        <!-- 郵便番号 -->
+        <div class="mt-4">
+          <x-input-label for="postalCode" :value="__('郵便番号')" />
+
+          <x-text-input id="postalCode" class="block mt-1 w-full"
+                          type="text"
+                          name="postalCode" required autocomplete="postalCode" />
+
+          <x-input-error :messages="$errors->get('postalCode')" class="mt-2" />
+        </div>
+        <!-- 住所１ -->
+        <div class="mt-4">
+          <x-input-label for="address_1" :value="__('都道府県')" />
+
+          <x-text-input id="address_1" class="block mt-1 w-full"
+                          type="text"
+                          name="address_1" required autocomplete="address_1" />
+
+          <x-input-error :messages="$errors->get('address_1')" class="mt-2" />
+        </div>
+        <!-- 住所２ -->
+        <div class="mt-4">
+          <x-input-label for="address_2" :value="__('市区町村')" />
+
+          <x-text-input id="address_2" class="block mt-1 w-full"
+                          type="text"
+                          name="address_2" required autocomplete="address_2" />
+
+          <x-input-error :messages="$errors->get('address_2')" class="mt-2" />
+        </div>
+        <!-- 住所３ -->
+        <div class="mt-4">
+          <x-input-label for="address_3" :value="__('番地以降')" />
+
+          <x-text-input id="address_3" class="block mt-1 w-full"
+                          type="text"
+                          name="address_3" required autocomplete="address_3" />
+
+          <x-input-error :messages="$errors->get('address_3')" class="mt-2" />
+        </div>
+        <!-- 電話番号 -->
+        <div class="mt-4">
+          <x-input-label for="phoneNumber" :value="__('電話番号')" />
+
+          <x-text-input id="phoneNumber" class="block mt-1 w-full"
+                          type="text"
+                          name="phoneNumber" required autocomplete="phoneNumber" />
+
+          <x-input-error :messages="$errors->get('phoneNumber')" class="mt-2" />
+        </div>
+        
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
