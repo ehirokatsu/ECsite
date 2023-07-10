bc1fb4335477673ab493b0df7bce153d4cfd368d
hiro <hiro>
2023-07-10 15:58:03 +0900

    トップ画面　レイアウト修正。設計書追記

--- resources/views/index.blade copy.php
+++ resources/views/index.blade copy.php
@@ -10,15 +10,28 @@
 
               </div>
               <h1 class="text-xl text-green-100">Hello World</h1>
-              @foreach($products as $product)
-              <img class="max-h-40" src="/storage/{{$product->image}}">
-                      商品名 : {{ $product->name }}
-                      単価 : {{ $product->cost }}
 
+              @foreach($products as $product)
+              <div class="grid grid-cols-12">
+                <div class="col-span-3">
+                </div>
+                <div class="col-span-3">
+                  <img class="max-h-40" src="/storage/{{$product->image}}">
+                </div>
+                <div class="col-span-3">
+                  <div class="grid grid-cols-1 gap-2">
+                    <div class="col-span-1">
+                      商品名 : {{ $product->name }}
+                    </div>
+                    <div class="col-span-1">
+                      単価 : {{ $product->cost }}
+                    </div>
+                    <div class="col-span-1">
                       <a href="{{ route('show', ['id' => $product->id]) }}" class="" >
                         詳細
                       </a>
-
+                    </div>
+                    <div class="col-span-1">
                       <form action="{{ route('cart.store') }}" method="post">
                         @csrf
                         <input type="hidden" name="id" value="{{ $product->id }}">
@@ -26,12 +39,23 @@
                           カートに入れる
                         </x-primary-button>
                       </form>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-span-3">
+                </div>
+
+                <div class="col-span-3">
+                </div>
                 @can('admin')
+                <div class="col-span-3">
                   <a href="{{ route('edit', ['id' => $product->id]) }}" class="" >
                     <x-primary-button>
                       編集
                     </x-primary-button>
                   </a>
+                </div>
+                <div class="col-span-3">
                   <form action="{{ route('destroy', ['id' => $product->id]) }}" method="post">
                     @method('DELETE')
                     @csrf
@@ -39,8 +63,11 @@
                       削除
                     </x-danger-button>
                   </form>
-
+                </div>
                 @endcan
+                <div class="col-span-3">
+                </div>
+              </div>
               <br>
               @endforeach
           </div>
