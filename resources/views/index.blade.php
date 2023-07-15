6d1e7ad60a3700b1b6ff222dee627baceb23ad8b
hiro <48009332+ehirokatsu@users.noreply.github.com>
2023-07-15 09:14:03 +0900

    トップ画面CSS修正中

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,22 +10,48 @@
 
               </div>
               <h1 class="text-xl text-green-10">Hello World</h1>
-              <div class="flex w-130 border">aaaa</div>
+              <div class="flex flex-wrap">
+                <h1 class="border flex-auto text-lg font-semibold text-slate-900">
+                  Classic Utility Jacket
+                </h1>
+                <div class="text-lg font-semibold text-slate-500">
+                  $110.00
+                </div>
+                <div class="w-full flex-none text-sm font-medium text-slate-700 mt-2">
+                  In stock
+                </div>
+              </div>
+              <span class="w-50 border">h222222</span>
+              <div class="flex flex-wrap">
+                <div class="bg-cyan-300 flex-auto">Item1</div>
+                <div class="bg-gray-500">Item2</div>
+                <div class="bg-lime-400 w-full">Item3</div>
+                <div class="bg-purple-300">Item4</div>
+              </div>
+              <div class="flex">
+                <div class="flex-auto flex">
+                  <div>item1</div>
+                  <div>item2</div>
+                </div>
+                <div>
+                  Item3
+                </div>
+              </div>
               @foreach($products as $product)
 
               <div class="flex">
                 <div class="flex-none">
                   <img class="max-h-40" src="/storage/{{$product->image}}">
                 </div>
-                <div class="flex-auto border">
+                <div class="flex-wrap border">
                   <div class="">
-                    <div class="border">
+                    <div class="border p-4">
                       商品名 : {{ $product->name }}
                     </div>
-                    <div class="border">
+                    <div class="border p-4">
                       単価 : {{ $product->cost }}
                     </div>
-                    <div class="border">
+                    <div class="border p-4">
                       <a href="{{ route('show', ['id' => $product->id]) }}" class="" >
                         詳細
                       </a>
