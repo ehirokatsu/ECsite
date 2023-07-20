3bf1debf84e2244a55a94167a537f03f01d15860
hiro <hiro>
2023-07-20 15:44:08 +0900

    Win環境ではfakerに失敗することがあるので修正

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 use Illuminate\Support\Str;
 use Carbon\Carbon;
-
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
  */
@@ -20,13 +21,18 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         //画像を生成する
-        $oldPath = $this->faker->image(storage_path('app/test'), 640, 480);
+        //$oldPath = $this->faker->image(storage_path('app/test'), 640, 480);
 
-        //ファイル名を作成する
+        //Windowsだとfaker->imageに失敗することがあるので下記方法にする
+        Storage::fake('test_images');
+        $imageName = 'test.jpg';
+        //C:\~\AppData\Local\Temp\php〇〇.tmpに保存される
+        $oldPath = UploadedFile::fake()->image($imageName);
+
+        //保存場所、ファイル名を変更する
         $newPath = storage_path('app/test/') . Carbon::now()->format('Y_m_d_H_i_s') . '_test_' . \Str::random(5) . '.jpg';
-
-        //fakerだとファイル名を指定できないのでrenameをする
         rename($oldPath, $newPath);
+
         //\Log::info($imagePath);
         $imageFilename = basename($newPath);
 
