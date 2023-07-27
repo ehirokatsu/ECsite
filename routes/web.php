dc0ef57e9eab171ae2d015349ba88b6da8e9b842
hiro <48009332+ehirokatsu@users.noreply.github.com>
2023-07-27 21:59:56 +0900

    Breeze vueをインストール、初期設定

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,11 +11,19 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});    
 //商品一覧画面
 Route::get('/', 'App\Http\Controllers\ShopController@index')->name('index');
 
@@ -104,24 +114,21 @@ Route::get('/orderHistory', 'App\Http\Controllers\OrderHistoryController@index')
 //注文履歴詳細画面
 Route::get('/orderHistory/{id}', 'App\Http\Controllers\OrderHistoryController@show')->name('orderHistory.show')->where('id', '[0-9]+');
 
+
+
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    
-});
-
-Route::get('/welcome', function () {
-    return view('welcome');
 });
 
 Route::get('/htmlTest', function () {
     return view('htmlTest');
 })->name('htmlTest');
+
 
 require __DIR__.'/auth.php';
