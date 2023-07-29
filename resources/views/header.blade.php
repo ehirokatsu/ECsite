c9163161933cb1bc5b1a00b9ca70381dcde95635
hiro <48009332+ehirokatsu@users.noreply.github.com>
2023-07-29 22:33:36 +0900

    ヘッダー、トップページのレイアウト修正

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,21 +1,32 @@
-<h2 class="font-semibold text-xl text-gray-800 leading-tight">
-    ECsite
-</h2>
-<a href="/" class="" >トップ</a>
-<a href="/cart" class="" >カート</a>
-<a href="/profile" class="" >アカウント編集</a>
-<a href="/contact" class="" >問合せ</a>
-@can('admin')
-<a href="/create" class="" >商品追加</a>
-<a href="/databaseManage" class="" >DBメンテ</a>
-@endcan
-<a href="{{ route('orderHistory') }}">注文履歴</a>
-<a href="{{ route('register') }}">登録</a>
-<a href="{{ route('login') }}">ログイン</a>
-<div class="flex">
-  <form method="POST" action="{{ route('logout') }}">
-    @csrf
-    <input type="submit" value="ログアウト">
-  </form>
+<div class="flex justify-between">
+  <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+      MyECサイト
+  </h2>
+  @if(isset(Auth::user()->name))
+    <h2>ようこそ {{ Auth::user()->name }} さま</h2>
+  @else
+    <h2>ようこそ ゲスト さま</h2>
+  @endif<!--追加-->
 </div>
-<a href="{{ route('htmlTest') }}">HTMLテスト</a>
+<div class="flex justify-between">
+  <div class="flex">
+    <a href="/" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded">トップ</a>
+    <a href="/cart" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded" >カート</a>
+    <a href="{{ route('orderHistory') }}" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded" >注文履歴</a>
+    <a href="/profile" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded" >アカウント編集</a>
+    <a href="/contact" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded" >問合せ</a>
+    @can('admin')
+    <a href="/create" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded" >商品追加</a>
+    <a href="/databaseManage" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded" >DBメンテ</a>
+    @endcan
+    <a href="{{ route('register') }}" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded" >商品登録</a>
+    <a href="{{ route('htmlTest') }}" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded" >HTMLテスト</a>
+  </div>
+  <div class="flex justify-end">
+    <a href="{{ route('login') }}" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded" >ログイン</a>
+    <form method="POST" action="{{ route('logout') }}">
+      @csrf
+      <input type="submit" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded"  value="ログアウト">
+    </form>
+  </div>
+</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,57 +6,50 @@
   <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             @foreach($products as $product)
-            <div class="flex p-2 border rounded-md justify-center">
-              <div class="">
-                <img class="max-h-60 max-w-60" src="/storage/{{$product->image}}">
-              </div>
-              <div class="">
-                <div class="flex border-b">
-                  <div class="p-4">
-                    商品名 : {{ $product->name }}
+              <div class="border border-gray-300 p-4 rounded-md">
+                <a href="{{ route('show', ['id' => $product->id]) }}" class="" >
+                  <img alt="商品画像" class="w-50 h-50 mb-2 rounded-md" src="/storage/{{$product->image}}">
+                </a>
+                <div class="flex justify-between">
+                  <div>
+                    <h2 class="text-lg font-semibold mb-2">{{ $product->name }}</h2>
+                    <p class="text-gray-600 mb-2">¥{{ $product->cost }}</p>
                   </div>
-                  <div class="p-4 text-lg font-semibold text-slate-500">
-                    単価 : {{ $product->cost }}
-                  </div>
-                </div>
-                <div class="p-4 text-sm font-medium text-slate-700 mt-2">
-                  <a href="{{ route('show', ['id' => $product->id]) }}" class="" >
-                    詳細
-                  </a>
-                </div>
-                <div class="p-3">
-                  <form action="{{ route('cart.store') }}" method="post">
-                    @csrf
-                    <input type="hidden" name="id" value="{{ $product->id }}">
-                    <x-primary-button type="submit" class="">
-                      カートに入れる
-                    </x-primary-button>
-                  </form>
-                </div>
-                @can('admin')
-                <div class="flex p-3">
-                  <div class="p-1">
-                    <a href="{{ route('edit', ['id' => $product->id]) }}" class="" >
-                      <x-primary-button>
-                        編集
-                      </x-primary-button>
-                    </a>
-                  </div>
-                  <div class="p-1">
-                    <form action="{{ route('destroy', ['id' => $product->id]) }}" method="post">
-                      @method('DELETE')
+                  <div class="">
+                    <form action="{{ route('cart.store') }}" method="post">
                       @csrf
-                      <x-danger-button type="submit" class="">
-                        削除
-                      </x-danger-button>
+                      <input type="hidden" name="id" value="{{ $product->id }}">
+                      <x-primary-button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-2 py-2 rounded">>
+                        カートに入れる
+                      </x-primary-button>
                     </form>
+                    @can('admin')
+                    <div class="flex p-2">
+                      <div class="p-1">
+                        <a href="{{ route('edit', ['id' => $product->id]) }}" class="" >
+                          <x-primary-button>
+                            編集
+                          </x-primary-button>
+                        </a>
+                      </div>
+                      <div class="p-1">
+                        <form action="{{ route('destroy', ['id' => $product->id]) }}" method="post">
+                          @method('DELETE')
+                          @csrf
+                          <x-danger-button type="submit" class="">
+                            削除
+                          </x-danger-button>
+                        </form>
+                      </div>
+                    </div>
+                    @endcan
                   </div>
                 </div>
-                @endcan
               </div>
+            @endforeach
             </div>
-          @endforeach
           </div>
       </div>
   </div>
