dd6b3bc4a758739276a16ca1ed08757de59779b9
hiro <48009332+ehirokatsu@users.noreply.github.com>
2023-07-30 15:32:22 +0900

    問合せフォームのレイアウト修正

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,28 +1,39 @@
-@extends('contact.layout')
+<x-app-layout>
+  <x-slot name="header">
+    @include('header')
+  </x-slot>
 
-@section('title', '問い合わせフォーム')
+  <div class="py-12">
+      <div class="max-w-lg mx-auto sm:px-6 lg:px-8 ">
+          <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+              <div class="p-6 text-gray-900  flex justify-center items-center">
+                <form method="post" action="{{ route('contact.confirm') }}">
+                  @csrf
+                  <div class="">
+                    <x-input-label for="email" />メールアドレス
+                    <x-text-input id="email" class="block mt-1 w-80" type="text" name="email" :value="old('email')" required autofocus />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                  </div>
 
-@section('content')
-  <form method="post" action="{{ route('contact.confirm') }}">
-    @csrf
-    <label for="">メールアドレス</label>
-    <input type="text" name="email" value="{{ old('email') }}">
-    @if ( $errors->has('email') )
-      <p class="error-message">{{ $errors->first('email') }}</p>
-    @endif
-    <br>
-    <label for="">タイトル</label>
-    <input type="text" name="title" value="{{ old('title') }}">
-    @if ( $errors->has('title') )
-      <p class="error-message">{{ $errors->first('title') }}</p>
-    @endif
-    <br>
-    <label for="">お問い合わせ内容</label>
-    <textarea name="body" id="" cols="30" rows="10">{{ old('body') }}</textarea>
-    @if ( $errors->has('body') )
-      <p class="error-message">{{ $errors->first('body') }}</p>
-    @endif
-    <br>
-    <button type="submit">入力内容確認</button>
-  </form>
-@endsection
+                  <div>
+                    <x-input-label for="title" />件名
+                    <x-text-input id="title" class="block mt-1 w-80" type="text" name="title" :value="old('title')" required autofocus />
+                    <x-input-error :messages="$errors->get('title')" class="mt-2" />
+                  </div>
+
+
+                  <div>
+                    <x-input-label for="body" />本文
+                    <textarea id="body" class="block mt-1 w-80 p-3 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" name="body" :value="old('body')" required autofocus></textarea>
+                    <x-input-error :messages="$errors->get('body')" class="mt-2" />
+                  </div>
+
+                  <div class="flex justify-end mt-4">
+                    <x-primary-button type="submit" >入力内容確認</x-primary-button>
+                  </div>
+                </form>
+              </div>
+          </div>
+      </div>
+  </div>
+</x-app-layout>
