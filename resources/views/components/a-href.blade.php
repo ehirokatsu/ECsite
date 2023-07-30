98f230aea60628bb81ccf51d3a401bc8eaa032db
hiro <48009332+ehirokatsu@users.noreply.github.com>
2023-07-30 14:52:13 +0900

    ヘッダーのリンクをコンポーネント化

---
+++ resources/views/components/a-href.blade.php
@@ -0,0 +1,3 @@
+<a href="{{ $href }}" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded">
+  {{ $slot }}
+</a>
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -6,27 +6,32 @@
     <h2>ようこそ {{ Auth::user()->name }} さま</h2>
   @else
     <h2>ようこそ ゲスト さま</h2>
-  @endif<!--追加-->
+  @endif
 </div>
 <div class="flex justify-between">
   <div class="flex">
-    <a href="/" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded">トップ</a>
-    <a href="/cart" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded" >カート</a>
-    <a href="{{ route('orderHistory') }}" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded" >注文履歴</a>
-    <a href="/profile" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded" >アカウント編集</a>
-    <a href="/contact" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded" >問合せ</a>
+    <x-a-href href="/" class="">トップ</x-a-href>
+    <x-a-href href="/cart" class="" >カート</x-a-href>
+    <x-a-href href="/contact" class="" >問合せ</x-a-href>
+    @if(isset(Auth::user()->name))
+      <x-a-href href="{{ route('orderHistory') }}" class="" >注文履歴</x-a-href>
+      <x-a-href href="/profile" class="" >アカウント編集</x-a-href>
+    @endif
     @can('admin')
-    <a href="/create" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded" >商品追加</a>
-    <a href="/databaseManage" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded" >DBメンテ</a>
+      <x-a-href href="/create" class="" >商品追加</x-a-href>
+      <x-a-href href="/databaseManage" class="" >DBメンテ</x-a-href>
     @endcan
-    <a href="{{ route('register') }}" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded" >商品登録</a>
-    <a href="{{ route('htmlTest') }}" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded" >HTMLテスト</a>
+    <x-a-href href="{{ route('htmlTest') }}" class="" >HTMLテスト</x-a-href>
   </div>
   <div class="flex justify-end">
-    <a href="{{ route('login') }}" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded" >ログイン</a>
-    <form method="POST" action="{{ route('logout') }}">
-      @csrf
-      <input type="submit" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded"  value="ログアウト">
-    </form>
+    @if(isset(Auth::user()->name))
+      <form method="POST" action="{{ route('logout') }}">
+        @csrf
+        <input type="submit" class="p-2 text-gray-500 hover:bg-gray-200 hover:text-black hover:font-bold rounded"  value="ログアウト">
+      </form>
+    @else
+    <x-a-href href="{{ route('register') }}" class="" >登録</x-a-href>
+      <x-a-href href="{{ route('login') }}" class="" >ログイン</x-a-href>
+    @endif
   </div>
 </div>
