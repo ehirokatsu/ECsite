b2c343ca7103a75ca3c8ccf13cffa39faf4acd1b
hiro <48009332+ehirokatsu@users.noreply.github.com>
2023-07-30 14:51:54 +0900

    カート画面のレイアウト修正

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -5,10 +5,7 @@
 
   <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-              <div class="p-6 text-gray-900">
-              </div>
-             
+          <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">            
               @if (!empty($carts))
                 <div class="flex">
                   <x-primary-button class="m-2">
@@ -25,45 +22,35 @@
                   </div>
                 </div>
                 @foreach($carts as $cart)
-                  <div class="flex p-2 border rounded-md justify-center">
+                  <div class="flex p-2 rounded-md justify-center">
                     <div class="">
-                      <img class="max-h-60 max-w-60" src="/storage/{{$cart['product']->image}}">
+                      <img src="/storage/{{$cart['product']->image}}"  class="w-40 h-auto">
+                    </div>
+                    <div class="mr-20">
+                      <div class="p-4">
+                        <h2 class="text-lg font-semibold mb-2">{{ $cart['product']->name }}</h2>
+                        <p class="text-gray-600 mb-2">¥{{ $cart['product']->cost }}</p>
+                      </div>
                     </div>
                     <div class="">
-                      <div class="flex border-b">
-                        <div class="p-4">
-                          <span>商品名 :</span>
-                          {{ $cart['product']->name }}
-                        </div>
-                        <div class="p-4 text-lg font-semibold text-slate-500">
-                          <span>単価 : </span>
-                          {{ $cart['product']->cost }}
-                        </div>
-                      </div>
-                      <div class="">
-                        <div class="p-3">
-                          <form action="{{ route('cart.quantityUpdate', ['id' => $cart['product']['id']]) }}" method="post">
-                            @method('PUT')
-                            @csrf
-                            <div>
-                              <label for="">個数</label>
-                              <input type="text" name="quantity" value="{{ $cart['quantity'] }}">
-                              <x-input-error :messages="$errors->get('quantity')" class="mt-2" />
-                            </div>
-                            <div class="m-4">
-                              <x-secondary-button type="submit" class="">
-                                更新する
-                              </x-secondary-button>
-                            </div>
-                          </form>
-                          <form action="{{ route('cart.destroy', ['id' => $cart['product']['id']]) }}" method="post">
-                            @method('DELETE')
-                            @csrf
-                            <x-danger-button type="submit" class="m-4">
-                              削除する
-                            </x-danger-button>
-                          </form>
-                        </div>
+                      <div class="flex p-3">
+                        <form action="{{ route('cart.quantityUpdate', ['id' => $cart['product']['id']]) }}" method="post">
+                          @method('PUT')
+                          @csrf
+                            <label for="">個数</label>
+                            <input type="number" name="quantity" class="w-20" value="{{ $cart['quantity'] }}">
+                            <x-input-error :messages="$errors->get('quantity')" class="mt-2" />
+                            <x-secondary-button type="submit" class=" m-2">
+                              更新する
+                            </x-secondary-button>
+                        </form>
+                        <form action="{{ route('cart.destroy', ['id' => $cart['product']['id']]) }}" method="post">
+                          @method('DELETE')
+                          @csrf
+                          <x-danger-button type="submit" class="m-2">
+                            削除する
+                          </x-danger-button>
+                        </form>
                       </div>
                     </div>
                   </div>
