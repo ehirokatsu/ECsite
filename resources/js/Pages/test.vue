6e45b3bd42cbb4d93e71ca8f7407a8ba4b43eac9
hiro <hiro>
2023-08-07 16:01:22 +0900

    Vue computed

--- resources/js/Pages/test.vue
+++ resources/js/Pages/test.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import {ref, computed} from "vue";
 
 const tmp = "123";
 const name = ref("test");
@@ -19,17 +19,29 @@ setInterval(changeTime, 1000);
 
 
 let radius = 1;
+const radiusRef = ref(radius);
 let result = 1;
 const resultRef = ref(result);
 const PI = 3.14;
 
+const resultComputed = computed(
+        (): number => {
+            return radiusRef.value * radiusRef.value * PI;
+        //return radius * radius * PI;
+    }
+);
 
-function changeRadius(): void {
-    radius = Math.round(Math.random() * 10);
-    result = radius * radius * PI;
-    resultRef.value = radius * radius * PI;
-}
-setInterval(changeRadius, 1000);
+
+setInterval(
+    (): void => {
+        radius = Math.round(Math.random() * 10);
+        radiusRef.value = radius;
+
+        result = radius * radius * PI;
+        resultRef.value = radius * radius * PI;
+    },
+    1000
+);
 
 
 
@@ -41,7 +53,7 @@ setInterval(changeRadius, 1000);
 <h2>{{ tmp }}</h2>
 <p>現在時刻：{{ timeStr }}</p>
 <p>現在時刻(Ref):{{ timeStrRef }}</p>
-<p>半径{{ radius }}の円の面積は円周率{{ PI }}で計算すると{{ result }}。{{ resultRef }}</p>
+<p>半径{{ radius }}の円の面積は円周率{{ PI }}で計算すると{{ result }}。{{ resultRef }}。{{ resultComputed }}</p>
 </template>
 
 <style>
