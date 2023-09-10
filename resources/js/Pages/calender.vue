d060389a16f51029f43d9a67239d646ec3edc929
hiro <e.hirokatsu@gmail.com>
2023-09-10 17:05:48 +0900

    vueカレンダー　Date関数

--- resources/js/Pages/calender.vue
+++ resources/js/Pages/calender.vue
@@ -2,12 +2,17 @@
 import {ref, computed} from "vue";
 import Index from "./index.vue";
 
+const now = new Date();
+//const nowRef = ref(now);
+const year = now.getFullYear();
+
 
 </script>
 
 <template>
 <h1>Calender</h1>
-
+{{ now }}
+{{ year }}
 
 </template>
 
