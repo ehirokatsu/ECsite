95fe0e95329215ccc40c73d5617334a7df37f85a
hiro <48009332+ehirokatsu@users.noreply.github.com>
2023-09-23 23:55:46 +0900

    コメントを追記

--- resources/js/Pages/snake.vue
+++ resources/js/Pages/snake.vue
@@ -1,26 +1,41 @@
 <script setup lang="ts">
 import {ref, computed, onMounted, watch } from "vue";
 
+//画面サイズの横幅
 const width = 10;
+
+//蛇の頭の初期位置
 const headPosition:{[key:string]:number} = {
     x: 4,
     y: 3,
 };
 const headPositionRef = ref(headPosition);
+
+//蛇が動くスピード（msec）
 let speed: number = 1000;
+
+//蛇が動く向きの初期値
 let direction: string = "→";
+
+//餌の初期位置
 const fruitPosition: number = 0;
 const fruitPositionRef = ref(fruitPosition);
+
+//蛇の長さ（頭を含める）
 let snakeLength: number = 2;
+
+//蛇の身体の位置
 const snakeBody: number[] = [];
 const snakeBodyRef = ref(snakeBody);//ref([])だと、includesでエラーが発生する
 
+//蛇が動ける範囲
 const fieldsRef = computed(
     (): number => {
         return width * width;
     }
 );
 
+//蛇の位置がフレームアウトしたかを判定する
 const frameOutRef = computed(
     () => {
         const x = headPositionRef.value.x;
@@ -30,18 +45,21 @@ const frameOutRef = computed(
     }
 ); 
 
+//蛇が自分の身体を食べたかを判定する
 const collidedRef = computed(
     () => {
         return snakeBodyRef.value.includes(snakeHeadRef.value);
     }
 );
 
+//ゲームオーバーを判定する
 const gameOverRef = computed(
     () => {
         return collidedRef.value || frameOutRef.value
     }
 );
 
+//蛇の頭の位置をフィールドに当てはめる
 const snakeHeadRef = computed(
     (): number => {
 
@@ -53,14 +71,17 @@ const snakeHeadRef = computed(
     }
 );
 
+//蛇が食べた餌の数
 const scoreRef = computed(
     (): number => {
         return snakeBodyRef.value.length - 1;
     }
 );
 
+//蛇を動かす処理
 const forwardSnake = ():void => {
 
+    //蛇の身体を長さに応じて増やす
     if (snakeBodyRef.value.length < snakeLength) {
         snakeBodyRef.value.push(snakeHeadRef.value);
     }
@@ -78,13 +99,14 @@ const forwardSnake = ():void => {
         case "↓": headPositionRef.value.y++;
         break;
     }
-    //console.log(snakeHead);
 
+    //蛇の長さが4以上になったらスピードを上げる
     if (snakeLength == 4) {
         speed = 700;
     }
 }
 
+//蛇を動かす処理をspeed毎に行う
 const moveSnake = (): void => {
     if (gameOverRef.value) {
         return;
@@ -94,7 +116,7 @@ const moveSnake = (): void => {
     setTimeout(moveSnake, speed);
 }
 
-
+//キーダウンイベントハンドラ
 const keydownEvent = (e: KeyboardEvent): void => {
     //console.log(e.key);
     switch (e.key) {
@@ -109,10 +131,12 @@ const keydownEvent = (e: KeyboardEvent): void => {
     }
 }
 
+//餌の位置をランダムに配置する
 const randomFruit = ():void => {
     fruitPositionRef.value = Math.floor(Math.random() * fieldsRef.value);
 }
 
+//ゲーム再スタート時の初期化処理
 const init = (): void => {
     headPositionRef.value.x = 3;
     headPositionRef.value.y = 4;
@@ -124,15 +148,23 @@ const init = (): void => {
     randomFruit();
 }
 
+//蛇の頭の位置が更新されるごとに行う処理
 watch(snakeHeadRef,
     ():void => {
+
+        //蛇が餌を食べた場合
         if (snakeHeadRef.value == fruitPositionRef.value) {
+
+            //餌の位置を更新する
             randomFruit();
+
+            //蛇の長さを増やす
             snakeLength++;
         }
     }
 );
 
+//WEBページがマウントされたときに行う初期化処理
 onMounted(
     () => {
       window.addEventListener('keydown', keydownEvent);
@@ -145,9 +177,14 @@ onMounted(
 
 <template>
 <div class="w-80">
+
     <p class="flex justify-center">蛇が食べたフルーツの数は{{ scoreRef }}です。</p>
+
     <div class="flex flex-wrap">
-        <div class="w-8 border bg-cyan-300" v-for="field in fieldsRef"
+
+        <div class="w-8 border bg-cyan-300"
+        v-for="field in fieldsRef"
+        v-bind:key="field"
         v-bind:class="{
             background: snakeHeadRef == field - 1,
             fruit: fruitPositionRef == field - 1,
