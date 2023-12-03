6b84ddf5bcb106c384310766f77874fe7f7a87b8
hiro <e.hirokatsu@gmail.com>
2023-12-03 15:30:55 +0900

    商品登録のルートにて、Bladeとvueのルートが被っていたので修正

--- routes/web.php
+++ routes/web.php
@@ -144,7 +144,7 @@ Route::get('/api/csrf-token', function () {
 
 Route::get('/vue', 'App\Http\Controllers\VueController@index')->name('vue.index');
 Route::get('/vue/create', 'App\Http\Controllers\VueController@create')->name('vue.create');
-Route::post('/', 'App\Http\Controllers\VueController@store')->name('vue.store');
+Route::post('/vue', 'App\Http\Controllers\VueController@store')->name('vue.store');
 Route::get('/vue/{id}/edit', 'App\Http\Controllers\VueController@edit')->name('vue.edit');
 Route::post('/vue/{id}', 'App\Http\Controllers\VueController@update')->name('vue.update');
 
