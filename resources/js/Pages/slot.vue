02ebb83864761ddde8e6b195b727c226071fbda7
hiro <48009332+ehirokatsu@users.noreply.github.com>
2024-01-05 14:10:40 +0900

    Replayボタンを追加

--- resources/js/Pages/slot.vue
+++ resources/js/Pages/slot.vue
@@ -17,9 +17,27 @@ let image3: string = images[0]//初期表示は「7」の画像。
 const imageRef3 = ref(image3);
 
 //SPINボタンを押下済か判定するフラグ。HTMLで使用するのでリアクティブにする。
-let isRunning: boolean = false;
-const isRunningRef = ref(isRunning)
+let isSpinRunning: boolean = false;
+const isSpinRunningRef = ref(isSpinRunning)
 
+let timeoutId1 = 0;
+let timeoutId2 = 0;
+let timeoutId3 = 0;
+
+//STOPボタンを押下済か判定するフラグ。HTMLで使用するのでリアクティブにする。
+let isStopSelected1: boolean = true;
+const isStopSelected1Ref = ref(isStopSelected1);
+let isStopSelected2: boolean = true;
+const isStopSelected2Ref = ref(isStopSelected2);
+let isStopSelected3: boolean = true;
+const isStopSelected3Ref = ref(isStopSelected3);
+
+//REPLAYボタンを押下済か判定するフラグ。HTMLで使用するのでリアクティブにする。
+//初期表示では押下不可のためtrueにしておく
+let isReplay: boolean = true;
+const isReplayRef = ref(isReplay);
+
+//SPINボタン押下後に画像を取得する処理
 const getRandomImage1 = (): void => {
     imageRef1.value = images[Math.floor(Math.random() * images.length)]
 }
@@ -29,21 +47,22 @@ const getRandomImage2 = (): void => {
 const getRandomImage3 = (): void => {
     imageRef3.value = images[Math.floor(Math.random() * images.length)]
 }
-let timeoutId1 = 0;
-let timeoutId2 = 0;
-let timeoutId3 = 0;
 
-let isSelected1: boolean = false;
-const isSelected1Ref = ref(isSelected1);
-let isSelected2: boolean = false;
-const isSelected2Ref = ref(isSelected2);
-let isSelected3: boolean = false;
-const isSelected3Ref = ref(isSelected3);
-
+//SPINボタン押下時の処理
 const spin = (): void => {
 
+    //SPINボタンを押下済みなら、2度押ししても何もしない
+    if (isSpinRunningRef.value) {
+        return;
+    }
+
     //SPINボタンが押下されたのでボタンを半透明にするフラグを立てる
-    isRunningRef.value = true;
+    isSpinRunningRef.value = true;
+
+    //STOPボタンを押下可能にする
+    isStopSelected1Ref.value = false;
+    isStopSelected2Ref.value = false;
+    isStopSelected3Ref.value = false;
 
     timeoutId1 = setInterval(() => {
         getRandomImage1();
@@ -57,16 +76,68 @@ const spin = (): void => {
 }
 
 const stop1 = (): void => {
+
+    //押下済なら2度押ししても何もしない
+    if (isStopSelected1Ref.value) {
+        return;
+    }
+
     clearInterval(timeoutId1);
-    isSelected1Ref.value = true;
+    isStopSelected1Ref.value = true;
+    isComplete();
 }
 const stop2 = (): void => {
+
+    //押下済なら2度押ししても何もしない
+    if (isStopSelected2Ref.value) {
+        return;
+    }
+
     clearInterval(timeoutId2);
-    isSelected2Ref.value = true;
+    isStopSelected2Ref.value = true;
+    isComplete();
 }
 const stop3 = (): void => {
+
+    //押下済なら2度押ししても何もしない
+    if (isStopSelected3Ref.value) {
+        return;
+    }
+
     clearInterval(timeoutId3);
-    isSelected3Ref.value = true;
+    isStopSelected3Ref.value = true;
+    isComplete();
+}
+
+//全てのSTOPボタンを押下したか判定する
+const isComplete = (): boolean => {
+
+    if (isStopSelected1Ref.value == true
+        && isStopSelected2Ref.value == true
+        && isStopSelected3Ref.value == true) {
+
+            //全て押下したらREPLAYボタンを押下可能にする
+            isReplayRef.value = false;
+            return true;
+    }
+    return false;
+}
+
+// Replayボタン押下時の処理
+const replay = (): void => {
+
+    //押下済なら2度押ししても何もしない
+    if (isReplayRef.value) {
+        return;
+    }
+
+    //SPIN,STOPボタンを押下可能に、REPLAYボタンを押下不可にする
+    isSpinRunningRef.value = false;
+    isStopSelected1Ref.value = true;
+    isStopSelected2Ref.value = true;
+    isStopSelected3Ref.value = true;
+    isReplayRef.value = true;
+
 }
 
 </script>
@@ -75,20 +146,21 @@ const stop3 = (): void => {
 <div class="main">
     <section class="panel">
         <img v-bind:src="imageRef1">
-        <div class="stop" v-bind:class="{inactive: isSelected1Ref}" v-on:click="stop1">STOP</div>
+        <div class="stop" v-bind:class="{inactive: isStopSelected1Ref}" v-on:click="stop1">STOP</div>
     </section>
     <section class="panel">
         <img v-bind:src="imageRef2">
-        <div class="stop" v-bind:class="{inactive: isSelected2Ref}" v-on:click="stop2">STOP</div>
+        <div class="stop" v-bind:class="{inactive: isStopSelected2Ref}" v-on:click="stop2">STOP</div>
     </section>
 
     <section class="panel">
         <img v-bind:src="imageRef3">
-        <div class="stop" v-bind:class="{inactive: isSelected3Ref}" v-on:click="stop3">STOP</div>
+        <div class="stop" v-bind:class="{inactive: isStopSelected3Ref}" v-on:click="stop3">STOP</div>
     </section>
     </div>
     <!--isRunningRefがtrueならinactiveクラスを付与する-->
-    <div class="spin" v-bind:class="{inactive: isRunningRef}" v-on:click="spin">SPIN</div>
+    <div class="spin" v-bind:class="{inactive: isSpinRunningRef}" v-on:click="spin">SPIN</div>
+    <div class="replay" v-bind:class="{inactive: isReplayRef}" v-on:click="replay">REPLAY</div>
 </template>
 
 <style>
@@ -141,9 +213,21 @@ const stop3 = (): void => {
         text-align: center;
         color: #fff;
         user-select: none;
-        margin: 0 auto;
+        margin:0  auto;
     }
-
+    .replay {
+        cursor: pointer;
+        width: 260px;
+        height: 36px;
+        background: #79d063;
+        box-shadow: 0 4px 0 #03ab28;
+        border-radius: 18px;
+        line-height: 36px;
+        text-align: center;
+        color: #fff;
+        user-select: none;
+        margin:10px  auto;
+    }
     .unmatched {
         opacity: 0.5;
     }
