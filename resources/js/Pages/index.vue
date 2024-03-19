7faecc7943f912674ac28b7ccc9820f45e6e4714
hiro <48009332+ehirokatsu@users.noreply.github.com>
2024-03-19 22:28:51 +0900

    微修正

--- resources/js/Pages/index.vue
+++ resources/js/Pages/index.vue
@@ -55,40 +55,40 @@ const submit = (id: number) => {
     <Head title="Dashboard" />
 
     <Header></Header>
-<!--
-    <GuestLayout>
-    -->
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    <div 
-                        v-for="product in products" 
-                        :key="product.id"
-                    >
-                        <div class="border border-gray-300 p-4 rounded-md">
-                            <div class="relative w-84 h-64 mb-3">
-                                <img class="absolute inset-0 w-full h-full object-cover" v-bind:src="'/storage/' + product.image" alt="">
-                            </div>
+
+    <div class="py-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                <div 
+                    v-for="product in products" 
+                    :key="product.id"
+                >
+                    <div class="border border-gray-300 p-4 rounded-md">
+                        <!--商品画像-->>
+                        <div class="relative w-84 h-64 mb-3">
+                            <img class="absolute inset-0 w-full h-full object-cover" v-bind:src="'/storage/' + product.image" alt="">
+                        </div>
+                        <!--説明箇所-->>
+                        <div class="flex justify-between">
                             <div>
                                 <h2 class="text-lg font-semibold mb-2">{{ product.name }}</h2>
                                 <p class="text-gray-600 mb-2">¥{{ product.cost }}</p>
                             </div>
                             <div class="">
                                 カートに入れる
-                            </div>
-                            <div class="flex p-2">
-                                <div class="p-1">
-                                    <!--文字列連結等の式を入れるにはbindが必要-->
-                                    <Link v-bind:href="route('vue.edit', {'id': product.id})">
-                                    <!--<a v-bind:href="'/vue/' + product.id +'/edit/'" class="" >-->
-                                        <PrimaryButton>編集</PrimaryButton>
-                                    </Link>
-                                </div>
-                                <div class="p-1">
-                                    <form @submit.prevent="submit(product.id)">
-                                        <DangerButton type="submit">削除</DangerButton>
-                                    </form>
+                                <div class="flex p-2">
+                                    <div class="p-1">
+                                        <!--文字列連結等の式を入れるにはbindが必要-->
+                                        <Link v-bind:href="route('vue.edit', {'id': product.id})">
+                                        <!--<a v-bind:href="'/vue/' + product.id +'/edit/'" class="" >-->
+                                            <PrimaryButton>編集</PrimaryButton>
+                                        </Link>
+                                    </div>
+                                    <div class="p-1">
+                                        <form @submit.prevent="submit(product.id)">
+                                            <DangerButton type="submit">削除</DangerButton>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -97,7 +97,5 @@ const submit = (id: number) => {
             </div>
         </div>
     </div>
-        <!--
-    </GuestLayout>
--->
+
 </template>
