32afa53dc168aa961bff75f23ce86cfe3ffba8e9
hiro <48009332+ehirokatsu@users.noreply.github.com>
2024-08-03 20:30:05 +0900

    Linkで使用しない処理を削除

--- resources/js/Pages/AjaxLink/index.vue
+++ resources/js/Pages/AjaxLink/index.vue
@@ -6,116 +6,16 @@ import InputLabel from '@/Components/InputLabel.vue';
 import InputError from '@/Components/InputError.vue';
 import TextInput from '@/Components/TextInput.vue';
 import { Link } from '@inertiajs/vue3';
-import axios from 'axios';
-import { ref, watch } from "vue";
-
-//apiでproductを取得する方法
-//let products = ref([]);
-//axios.get("/product").then(response => { products.value = response.data });
+import { ref } from "vue";
 
 //Inertia::renderでproductsを受け取る
-
 const props = defineProps({
     products: Object,
 });
 
-/*
-//axiosを使用する場合
-import axios from 'axios';
-import { ref, onMounted } from "vue";
-const csrfToken = ref('');
-
-//GETでCSRFトークン取得で、Blade経由なしで取得可能
-onMounted(async () => {
-    const response = await axios.get('/api/csrf-token');
-    csrfToken.value = response.data.token;
-    console.log(csrfToken.value);
-});
-
-function deleteButton(id: number): void {
-    axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken.value; // CSRFトークンをヘッダーにセット
-    axios.delete(route('destroy', id));
-}
-*/
-
-
-//deleteメソッドをフォームで使用する場合
-//delete送信用ダミー
-import { useForm } from '@inertiajs/vue3';
-
-const form = useForm({
-    
-});
-/*
-//CSRFなしでも削除できた
-const submit = (id: number) => {
-    ///axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken.value; // CSRFトークンをヘッダーにセット
-    //form.delete('/' + id);
-    //下記だと、URLが/id?id=となってしまう
-    //form.delete(route('destroy', ['id', id]));
-    form.delete(route('vue.destroy', {'id': id}));
-};
-*/
-
-/*
-import { usePage } from '@inertiajs/vue3'
-// ページプロパティの取得
-const page = usePage()
-//console.log(page.props.flash?.message)
-*/
-
-
-
-//検索機能 axiosで実装
+//検索キー用
 const searchWord = ref("");
-const searchResults = ref(props.products); // 商品のリアクティブなコピーを作成
 
-const ExecSearch = async () => {
-    try {
-        const response = await axios.get(route('vue.ajaxlink.search'), {
-            params: {
-                query: searchWord.value
-            }
-        });
-        searchResults.value = response.data;
-        //console.log(searchResults);
-    } catch (error) {
-        console.error("Search failed:", error);
-    }
-};
-
-/*
-//検索機能 useFormで実装。
-const searchForm = useForm({
-    search: ''
-});
-interface Product {
-    id: number;
-    name: string;
-    cost: number;
-    image: string;
-}
-
-const props = defineProps<{
-    products: Product[];
-}>();
-
-const searchWord = ref('');
-const searchResults = ref<Product[]>([...props.products]);
-
-watch(searchWord, () => {
-    searchForm.get(route('vue.search', { query: searchWord.value }), {
-        preserveState: true,
-        //戻り値はJSONではなくページ。indexと同様にpropsでproductを受け取る。
-        onSuccess: (page) => {
-            searchResults.value = (page.props.products as Product[]);
-        },
-        onError: (errors) => {
-            console.error(errors);
-        }
-    });
-});
-*/       
 </script>
 
 <template>
@@ -137,7 +37,7 @@ watch(searchWord, () => {
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 <div 
-                    v-for="product in searchResults" 
+                    v-for="product in props.products" 
                     :key="product.id"
                 >
                     <div class="border border-gray-300 p-4 rounded-md">
@@ -156,12 +56,7 @@ watch(searchWord, () => {
                                 <div class="flex p-2">
                                     <div class="p-1">
                                         <!--文字列連結等の式を入れるにはbindが必要-->
-                                        
                                         <Link v-bind:href="route('vue.ajaxlink.edit', {'id': product.id})">
-                                        <!--
-                                        <Link v-bind:href="route('vue.index')">
-                                            -->
-                                        <!--<a v-bind:href="'/vue/' + product.id +'/edit/'" class="" >-->
                                             <PrimaryButton>編集</PrimaryButton>
                                         </Link>
                                     </div>
